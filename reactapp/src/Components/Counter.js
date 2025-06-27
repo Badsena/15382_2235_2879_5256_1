8d53f3b4d5f6bd26a349808a85532f9f6117c43e
Badsena <sena@amypo.in>
2025-06-27 14:00:27 +0000

    User

--- reactapp/src/Components/Counter.js
+++ reactapp/src/Components/Counter.js
@@ -4,7 +4,7 @@ const Counter = () => {
   const [count, setCount] = useState(0);
 
   const handleIncrease = () => {
-    setCount(count+10);
+    setCount(count+1);
   };
 
   const handleDecrease = () => {
